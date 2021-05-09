97f8e42c9b4774f173f19e4b46e6dd342262e403
Italo Domingues <italo.domingues1@gmail.com>
2021-05-08 21:37:49 -0300

    update

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import {Container, Wrapper} from "./style";
 import {io} from 'socket.io-client'
 
-const socket = io('/', {transports: ['websocket']});
+const socket = io('http://localhost:4000', {transports: ['websocket']});
 
 const Chat = () => {
     const [mensagens, setMensagens] = useState([]);
